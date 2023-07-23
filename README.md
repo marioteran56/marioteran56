6badf224027bb156a067fafaa6889986a313d7ce
Mario Alberto Terán Acosta <marioteran1@hotmail.com>
2023-07-23 12:46:13 -0600

    GIF test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-<img src="https://i.gifer.com/72uG.gif" width="100%"/>
+<img src="https://i.gifer.com/72uG.gif" width="100%" style="border-radius:5px;"/>
+
+<p>Hello</p>
--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
+
 function generateReadMe() {
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
     if (err) throw err;
